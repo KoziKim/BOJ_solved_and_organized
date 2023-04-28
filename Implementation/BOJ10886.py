44ea1e4169d53a535854abf0866f96d27ece0d20
Kozi <126449657+KoziKim@users.noreply.github.com>
2023-04-28 23:04:19 +0900

    0 = not cute / 1 = cute

---
+++ Implementation/BOJ10886.py
@@ -0,0 +1,13 @@
+n = int(input())
+cute = 0
+not_cute = 0
+for i in range(n):
+    a = int(input())
+    if a == 1:
+        cute += 1
+    elif a == 0:
+        not_cute += 1
+if cute > not_cute:
+    print("Junhee is cute!")
+elif cute < not_cute:
+    print("Junhee is not cute!")
